69c85aa32aa3936fefb9db3a1cb760e38a55b37b
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2014-09-10 01:42:37 +0900

    Change ip address to arch workstation lan ip

--- src/rpi/joystick_client.py
+++ src/rpi/joystick_client.py
@@ -5,7 +5,7 @@ import pickle
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("localhost", 9000))
+    s.connect(("10.249.255.151", 9000))
     data = "body"
     while True:
         try:
