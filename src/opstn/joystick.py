e8d6abfe3679e8ccf86a2afb6eb58400a4167c5c
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2014-12-27 20:10:36 +0900

    Position changed to a class, with direction a read-only property.

--- src/opstn/joystick.py
+++ src/opstn/joystick.py
@@ -4,46 +4,60 @@ import pygame
 from collections import namedtuple
 
 
-def interpret_direction(pos, invert_vertical=False):
-    """Interpret joystick axis position values to obtain a direction.
+class Position(object):
+    """A class representing the joystick axis position.
 
-    The directions are Up, Down, Left, and Right, as well as their
-    intermediates.
-
-    Args:
-        pos: A tuple (x, y) containing the positions to be interpreted.
-            Up and Right are normally positive.
-        invert_vertical: (optional) Makes Down positive instead of Up. Default
-            is False.
-
-    Returns:
-        direction: "U", "D", "L", "R" if the motion is completely in a cardinal
-            direction, or a combination of two directions if motion is in an
-            ordinal direction. In those cases, the first letter would be "U" or
-            "D", and the second letter would be "L" or "R". If there is no
-            motion detected, return "none".
+    Attributes:
+            x: The x-position of the axis.
+            y: The y-position of the axis.
+            inverted: Whether the direction is y-inverted.
     """
+    def __init__(self, x, y, inverted=False):
+        """Inits position.
 
-    if pos[1] > 0:
-        vert = "D" if invert_vertical else "U"
-    elif pos[1] < 0:
-        vert = "U" if invert_vertical else "D"
-    else:
-        vert = ""
+        Args:
+            x: The x-position of the axis.
+            y: The y-position of the axis.
+            inverted: (optional) Whether the direction is y-inverted. Default
+                is False.
+        """
+        self.x = x
+        self.y = y
+        self.inverted = inverted
 
-    if pos[0] > 0:
-        hrz = "R"
-    elif pos[0] < 0:
-        hrz = "L"
-    else:
-        hrz = ""
+    @property
+    def direction(self):
+        """Determine the direction represented by the joystick axis position.
 
-    if vert == "" and hrz == "":
-        direction = "none"
-    else:
-        direction = vert + hrz
+        The directions are Up, Down, Left, and Right, and their intermediates.
 
-    return direction
+        Returns:
+            "U", "D", "L", "R" if the motion is completely in a cardinal
+                direction, or a combination of two directions if motion is in
+                an ordinal direction. In those cases, the first letter would be
+                "U" or "D", and the second letter would be "L" or "R". If there
+                is no motion detected, return "none".
+        """
+        if self.y > 0:
+            vert = "D" if self.inverted else "U"
+        elif self.y < 0:
+            vert = "U" if self.inverted else "D"
+        else:
+            vert = ""
+
+        if self.x > 0:
+            hrz = "R"
+        elif self.x < 0:
+            hrz = "L"
+        else:
+            hrz = ""
+
+        if vert == "" and hrz == "":
+            direction = "none"
+        else:
+            direction = vert + hrz
+
+        return direction
     
 
 def read_joystick(stick):
@@ -53,38 +67,32 @@ def read_joystick(stick):
     devices may have different configurations.
 
     Args:
-        stick: The controller that needs to be checked. Should be a pygame
+        stick: The joystick that needs to be checked. Should be a pygame
             joystick object.
 
     Returns:
         A named tuple containing:
-            buttons: The buttons that are pressed. This is a list containing a
-                human-readable representation of all the buttons pressed on the
-                controller.
-            dpad: A tuple containing the dpad position. The position is either
-                -1, 0, or 1.
-            lstick: A tuple (x, y) containing the positions for the left analog
-                stick. Ranges between -1 and 1.
-            rstick: A tuple (x, y) containing the positions for the right
-                analog stick. Ranges between -1 and 1.
+            buttons: A list containing a human-readable representation of all
+                the currently-depressed buttons on the controller.
+            dpad: The dpad position. Can contain either -1, 0, or 1.
+            lstick: The left analog stick position. Ranges from -1 to 1.
+            rstick: The right analog stick position. Ranges from -1 to 1.
     """
-    JoystickState = namedtuple("JoystickState",
-                               ["buttons", "dpad", "lstick", "rstick"])
-    Position = namedtuple("Position", ["x", "y"])
-    button_list = ("□", "×", "○", "△",  # 0-3
-                   "L1", "R1", "L2", "R2", # 4-7
-                   "select", "start",  # 8-9
-                   "L3", "R3")  # 10-11
+    State = namedtuple("State", ["buttons", "dpad", "lstick", "rstick"])
+    buttons = ("□", "×", "○", "△",  # 0-3
+               "L1", "R1", "L2", "R2", # 4-7
+               "select", "start",  # 8-9
+               "L3", "R3")  # 10-11
+    n_buttons = stick.get_numbuttons()
 
     pygame.event.pump()  # Synchronize pygame with computer (i.e. Refresh)
 
-    pressed = [button_list[i] for i in range(stick.get_numbuttons())
-               if stick.get_button(i) != 0]
-    dpad = stick.get_hat(0)
-    lstick = Position(stick.get_axis(0), -stick.get_axis(1))  # Make Up > 0.
-    rstick = Position(stick.get_axis(2), -stick.get_axis(3))  # Make Up > 0.
+    pressed = [buttons[i] for i in range(n_buttons) if stick.get_button(i) > 0]
+    dpad = Position(*stick.get_hat(0))  # Unpack the tuple.
+    lstick = Position(stick.get_axis(0), stick.get_axis(1), inverted=True)
+    rstick = Position(stick.get_axis(2), stick.get_axis(3), inverted=True)
 
-    state = JoystickState(pressed, dpad, lstick, rstick)
+    state = State(pressed, dpad, lstick, rstick)
     return state
 
 
@@ -109,7 +117,7 @@ if __name__ == "__main__":
     while True:
         try:
             pressed, dpad, lstick, rstick = read_joystick(stick)
-            out_1 = "dpad: {:4}".format(interpret_direction(dpad))
+            out_1 = "dpad: {:4}".format(dpad.direction)
             out_2 = "lstick: [{:5.2f}, {:5.2f}]".format(lstick.x, lstick.y)
             out_3 = "rstick: [{:5.2f}, {:5.2f}]".format(rstick.x, rstick.y)
             out_4 = "buttons: {:75}".format(str(pressed))
