11ac8aef5f19b149621241e097076797dcee741c
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-01-02 00:40:50 +0900

    Added docstring.

--- src/opstn/server.py
+++ src/opstn/server.py
@@ -8,18 +8,37 @@ import pickle
 
 
 class Server(object):
+    """Server class to communicate via TCP.
+
+    This class is capable of handling multiple clients simultaneously by
+    creating a subprocess handler for each new client.
+
+    Attributes:
+        logger: The logger for the main server class.
+        hostname: The hostname or IP Address of the server.
+        port: The port used for communication.
+        socket: The socket opened by the server.
+    """
     def __init__(self, hostname, port):
+        """Inits the server.
+
+        Args:
+            hostname: The hostname or IP Address of the server.
+            port: The port used for communication.
+        """
         self.logger = logging.getLogger("server")
         self.hostname = hostname
         self.port = port
 
     def start(self):
+        """Starts the server."""
         self.logger.debug("Listening...")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.hostname, self.port))
         self.socket.listen(1)
         
         while True:
+            """Fork a new process to deal with a new client."""
             conn, address = self.socket.accept()
             self.logger.debug("Got connection")
             process = mp.Process(target=self.handle, args=(conn, address))
@@ -28,6 +47,12 @@ class Server(object):
             self.logger.debug("Started process {}".format(process))
 
     def handle(self, connection, address):
+        """A client handler. A new one is started for each client.
+        
+        Args:
+            connection: The socket object to be used for communication.
+            address: The address of the socket object.
+        """
         logger = logging.getLogger("process-{}".format(address[1]))
         try:
             logger.debug("Connected to {}".format(address[0]))
@@ -57,7 +82,7 @@ class Server(object):
 
 
 if __name__=="__main__":
-    logging.basicConfig(level=logging.ERROR)
+    logging.basicConfig(level=logging.INFO)
     stick_body = joystick.Controller(0)
 
     server = Server("localhost", 9000)
@@ -65,11 +90,11 @@ if __name__=="__main__":
         logging.info("Starting server...")
         server.start()
     except KeyboardInterrupt:
-        logging.info("Keyboard interrupt received!")
+        logging.exception("Keyboard interrupt received!")
     finally:
         logging.info("Shutting down...")
         logging.info("Quitting joystick {}".format(stick_body.stick_id))
-        stick_body.quit()
+        joystick.Controller.quit_all()
         for process in mp.active_children():
             logging.info("Shutting down process {}".format(process))
             process.terminate()
