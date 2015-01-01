ab84c6bc94851ca3e87a83837e3f47196150e24e
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-01-01 14:59:13 +0900

    Joystick client example to talk to server.

---
+++ src/rpi/joystick_client.py
@@ -0,0 +1,16 @@
+import socket
+import pickle
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", 9000))
+    data = "body"
+    while True:
+        try:
+            s.sendall(data.encode())
+            result = s.recv(1024)
+            dpad, lstick, rstick, buttons = pickle.loads(result)
+            print(dpad, lstick, rstick, buttons)
+        except KeyboardInterrupt:
+            break
+    s.close()
