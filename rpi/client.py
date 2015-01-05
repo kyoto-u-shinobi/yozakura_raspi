ff4b86fcdcc644c87e621e8fb9f7b454a4f952be
masasin <mshtsy@gmail.com>
2015-01-06 01:56:33 +0900

    Fixing networking issues.

--- rpi/client.py
+++ rpi/client.py
@@ -11,16 +11,21 @@ if __name__ == "__main__":
     left_motor = motors.Motor(31, 33, 35, 37, "left_motor")
     right_motor = motors.Motor(32, 36, 38, 40, "right_motor")
 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("10.249.255.151", 9999))
     while True:
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(("localhost", 9999))
-            s.sendall(str.encode("sticks_y"))
+            #s.sendall(str.encode("body_sticks_y"))
+            #result = s.recv(1024)
+            #lstick_y, rstick_y = pickle.loads(result)
+            s.sendall(str.encode("body_state"))
             result = s.recv(1024)
-            lstick_y, rstick_y = pickle.loads(result)
-            logging.debug("{:9.7} {:9.7}".format(lstick_y, rstick_y))
-            left_motor.drive(-lstick_y)
-            right_motor.drive(-rstick_y)
+            state = pickle.loads(result)
+            print(state.human(), end="\r")
+
+            #logging.debug("{:9.7} {:9.7}".format(lstick_y, rstick_y))
+            #left_motor.drive(-lstick_y)
+            #right_motor.drive(-rstick_y)
         except KeyboardInterrupt:
             break
     motors.Motor.shut_down_all()
