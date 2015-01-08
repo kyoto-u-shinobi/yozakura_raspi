5d24deba453e2360d1026b928f8b5244773a475f
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-01-08 10:57:33 +0900

    Allow address reuse

--- common/networking.py
+++ common/networking.py
@@ -1,16 +1,17 @@
 import logging
 import socketserver
 
-class Handler(socketserver.BaseRequestHandler):
+class HandlerBase(socketserver.BaseRequestHandler):
     def __init__(self, request, client_address, server):
         self.logger = logging.getLogger("{}_handler".format(client_address[0]))
         self.logger.debug("New handler created")
         super().__init__(request, client_address, server)
 
-class Server(socketserver.ForkingMixIn, socketserver.TCPServer):
+
+class ServerBase(socketserver.ForkingMixIn, socketserver.TCPServer):
+    allow_reuse_address = True
     def __init__(self, server_address, handler_class):
         self.logger = logging.getLogger("{}_server".format(server_address[0]))
         self.logger.debug("Creating server")
         super().__init__(server_address, handler_class)
-        self.allow_reuse_address = True
         self.logger.info("Listening to port {}".format(server_address[1]))
--- rpi/client.py
+++ rpi/client.py
@@ -1,24 +1,24 @@
 # (C) 2015  Kyoto University Mechatronics Laboratory
 # Released under the GNU General Public License, version 3
-import motors
+from rpi import motors
 import socket
 import pickle
 import logging
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     left_motor = motors.Motor(31, 33, 35, 37, "left_motor")
     right_motor = motors.Motor(32, 36, 38, 40, "right_motor")
 
     while True:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(("10.249.255.151", 9000))
+            s.connect(("localhost", 9999))
             s.sendall(str.encode("sticks_y"))
             result = s.recv(1024)
             lstick_y, rstick_y = pickle.loads(result)
-            logging.info("{:9.7} {:9.7}".format(lstick_y, rstick_y))
+            logging.debug("{:9.7} {:9.7}".format(lstick_y, rstick_y))
             left_motor.drive(-lstick_y)
             right_motor.drive(-rstick_y)
         except KeyboardInterrupt:
