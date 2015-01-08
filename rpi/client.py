c96a23254e9f7cba87cba0ce71e22e361f40822e
zhang1chi <zhangyichi900421@sina.com>
2015-01-08 20:02:21 +0900

    Setup both motors to use hardware pwm.

--- rpi/client.py
+++ rpi/client.py
@@ -7,25 +7,21 @@ import logging
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    left_motor = motors.Motor(31, 33, 35, 37, "left_motor")
-    right_motor = motors.Motor(32, 36, 38, 40, "right_motor")
+    logging.basicConfig(level=logging.INFO)
+    left_motor = motors.Motor(12, 8, 10, 11, "left_motor"，hard=True, scaling=0.9)
+    right_motor = motors.Motor(35, 32, 36, 38, "right_motor", hard=True, scaling=0.9)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("10.249.255.151", 9999))
     while True:
         try:
-            #s.sendall(str.encode("body_sticks_y"))
-            #result = s.recv(1024)
-            #lstick_y, rstick_y = pickle.loads(result)
-            s.sendall(str.encode("body_state"))
+            s.sendall(str.encode("body_sticks_y"))
             result = s.recv(1024)
-            state = pickle.loads(result)
-            print(state.human(), end="\r")
+            lstick_y, rstick_y = pickle.loads(result)
 
-            #logging.debug("{:9.7} {:9.7}".format(lstick_y, rstick_y))
-            #left_motor.drive(-lstick_y)
-            #right_motor.drive(-rstick_y)
+            logging.debug("{:9.7} {:9.7}".format(lstick_y, rstick_y))
+            left_motor.drive(-lstick_y)
+            right_motor.drive(-rstick_y)
         except KeyboardInterrupt:
             break
     motors.Motor.shut_down_all()
