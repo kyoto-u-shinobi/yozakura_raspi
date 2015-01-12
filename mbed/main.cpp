d8a045ec7242a21ba31c91ed786b704c678a3228
yuusuke0126 <yuusuke.kobayashi.0126@gmail.com>
2015-01-12 10:30:38 +0900

    Compile to mbed

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -5,33 +5,32 @@
 Serial rpi(USBTX, USBRX);
 
 class Motor {
-  PwmOut enable;
-  DigitalOut pos, neg;
-  public:
-    Motor(PinName pin_enable, PinName pin_pos, PinName pin_neg) {
-      enable(pin_enable);
-      pos(pin_pos);
-      neg(pin_neg);
+  public:    
+    Motor(PinName pin_enable, PinName pin_pos, PinName pin_neg) : enable(pin_enable), pos(pin_pos),  neg(pin_neg) {
+      enable = pos = neg = 0;
     }
 
     void drive(float speed) {
       pos = (speed < 0 ? 0 : 1);
       neg = (speed < 0 ? 1 : 0);
-      enable = (speed < 0 ? -speed : speed);
+      enable = abs(speed);
     }
+    
+  private:
+    PwmOut enable;
+    DigitalOut pos, neg;
 };
 
 int main() {
-	Motor * motors;
-	motors = new Motor[4] { {p21, p11, p12},  // left motor
-				{p22, p13, p14},  // right motor
-				{p23, p27, p28},
-				{p24, p29, p30} };
+    Motor motors[4] = { Motor(p21, p11, p12),
+                        Motor(p22, p13, p14), 
+                        Motor(p23, p27, p28), 
+                        Motor(p24, p29, p30) };
 
   char c;
   int motor_id, sign, speed;
   while(1) {
-    c = pc.getc();
+    c = rpi.getc();
 
     motor_id = c >> 6;
     sign = (c & (1 << 5)) ? -1 : 1;
@@ -39,6 +38,4 @@ int main() {
 
     motors[motor_id].drive(sign * speed / 31);
   }
-
-  return 0;
 }
