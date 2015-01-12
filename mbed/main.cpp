533b980ff55ff9693031d3a82f45c9d2e0d417c4
mu--- <mu---@users.noreply.github.com>
2015-01-12 09:10:02 +0900

    Fix drive function.

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -15,29 +15,29 @@ class Motor {
     }
 
     void drive(float speed) {
-      pin_pos = (speed < 0 ? 0 : 1);
-      pin_pos = (speed < 0 ? 1 : 0);
-      enable = speed;
+      pos = (speed < 0 ? 0 : 1);
+      neg = (speed < 0 ? 1 : 0);
+      enable = (speed < 0 ? -speed : speed);
     }
 };
 
-Motor * motors;
-motors = new Motor[4] { {p21, p11, p12},  // left motor
-			{p22, p13, p14},  // right motor
-			{p23, p27, p28},
-			{p24, p29, p30} };
-
 int main() {
+	Motor * motors;
+	motors = new Motor[4] { {p21, p11, p12},  // left motor
+				{p22, p13, p14},  // right motor
+				{p23, p27, p28},
+				{p24, p29, p30} };
+
   char c;
-  int motor, sign, speed;
+  int motor_id, sign, speed;
   while(1) {
     c = pc.getc();
 
-    motor = c >> 6;
+    motor_id = c >> 6;
     sign = (c & (1 << 5)) ? -1 : 1;
     speed = c & 31;
 
-    motors[motor].drive(sign * speed / 31);
+    motors[motor_id].drive(sign * speed / 31);
   }
 
   return 0;
