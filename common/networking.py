335d5b216a8e91b6635040314a325fc7b3560471
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-01-14 13:10:59 +0900

    Add a function to get current IP address based on interface.

--- common/networking.py
+++ common/networking.py
@@ -1,5 +1,18 @@
 import logging
 import socketserver
+import socket
+import fcntl
+import struct
+
+
+def get_ip_address(interface):
+    """http://goo.gl/zdkOeZ"""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    packed = struct.pack("256s", str.encode(interface))
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(),
+                                        0x8915,  # SIOCGIFADDR
+                                        packed)[20:24])
+
 
 class HandlerBase(socketserver.BaseRequestHandler):
     def __init__(self, request, client_address, server):
@@ -14,3 +27,27 @@ class ServerBase(socketserver.ForkingMixIn, socketserver.TCPServer):
         super().__init__(server_address, handler_class)
         self.logger = logging.getLogger("{}_server".format(self.server_address[0]))
         self.logger.info("Listening to port {}".format(server_address[1]))
+
+
+class ClientBase(object):
+    def __init__(self, server_address):
+        try:
+            self.ip_address = get_ip_address("eth0")
+        except OSError:
+            self.ip_address = get_ip_address("enp2s0")
+        self.logger = logging.getLogger("{}_client".format(self.ip_address))
+        self.request = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.request.connect(server_address)
+
+    def send(self, string):
+        try:
+            self.request.sendall(str.encode(string))
+        except TypeError:  # Already bytecode
+            self.request.sendall(string)
+
+    def receive(self, length=1024):
+        return self.request.recv(length)
+
+    @abc.abstractmethod
+    def handle():
+        pass
