3ccdf5d57d8aacdad0033c6e9e90964108f9a38a
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-01-14 19:22:54 +0900

    Add docstring, and minor changes.
    self.data -> data
    self.reply -> reply

--- opstn/server.py
+++ opstn/server.py
@@ -6,43 +6,78 @@ import socket
 
 
 class Handler(HandlerBase):
+    """A handler for connection requests.
+    
+    Attributes:
+        request: A socket object handling communication with the client.
+    """
     def handle(self):
+        """Handle the requests.
+
+        Once connected to the client, the handler loops and keeps listening for
+        input. This allows us to find out when the client is disconnected, and
+        also allows for a much higher communication rate with the robot.
+
+        Inputs handled:
+            state: Reply with the state of the controller.
+            echo: Reply with what the client has said.
+            print: Reply with what the client has said, and print to screen.
+        """
         self.logger.info("Connected to client")
         self.request.settimeout(0.5)
 
         while True:
             try:
-                self.data = self.request.recv(1024).decode().strip()
+                data = self.request.recv(1024).decode().strip()
             except socket.timeout:
-                self.warning("Lost connection to robot")
-                self.info("Robot will shut down motors")
+                self.logger.warning("Lost connection to robot")
+                self.logger.info("Robot will shut down motors")
                 continue
-            self.logger.debug('Received: "{}"'.format(self.data))
-            if self.data == "":
+            self.logger.debug('Received: "{}"'.format(data))
+
+            if data == "":  # Client exited safely.
                 self.logger.info("Terminating client session")
                 break
-            if self.data == "body":
-                state = self.server.controllers["body"].get_state()
-                self.reply = pickle.dumps(state)
-            elif self.data.split()[0] == "echo":
-                self.reply = " ".join(self.data.split()[1:])
+
+            if data == "state":
+                state = self.server.controllers["wheels"].get_state()
+                reply = pickle.dumps(state)
+            elif data.split()[0] == "echo":
+                reply = " ".join(data.split()[1:])
+            elif data.split()[0] == "print":
+                reply = " ".join(data.split()[1:])
+                self.logger.info('Client says: "{}"'.format(reply))
             else:
-                self.reply = 'Unable to parse command: "{}"'.format(self.data)
+                reply = 'Unable to parse command: "{}"'.format(data)
+                self.logger.debug(reply)
+
             try:
-                self.request.sendall(str.encode(self.reply))
+                self.request.sendall(str.encode(reply))
             except TypeError:  # Already bytecode
-                self.request.sendall(self.reply)
+                self.request.sendall(reply)
 
 
 class Server(ServerBase):
-    def __init__(self, *args):
-        super().__init__(*args)
+    """A TCP Server"""
+    def __init__(self, *args, **kwargs):
+        """Inits the server."""
+        super().__init__(*args, **kwargs)
         self.controllers = {}
 
     def add_controller(self, controller):
+        """Register a controller.
+
+        Args:
+            controller: The controller to be registered.
+        """
         self.logger.debug("Adding controller {}".format(controller))
         self.controllers[controller.name] = controller
 
     def remove_controller(self, controller):
+        """Deregister a controller.
+
+        Args:
+            controller: The controller to be deregistered.
+        """
         self.logger.debug("Removing controller {}".format(controller))
         del self.controllers[controller.name]
