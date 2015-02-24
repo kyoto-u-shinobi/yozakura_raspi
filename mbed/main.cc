35d625c74eec65a0c5cf71f9cdf804703afdd1b8
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-02-24 22:18:24 +0900

    Modify motor class to support new motor driver

--- mbed/main.cc
+++ mbed/main.cc
@@ -26,61 +26,54 @@ union MotorPacket {
 
 // Class representing a motor driver.
 // 
-// Connect the Enable, PWMP, and PWMN pins to the mbed. The motor driver's
-// fault signal should go to the Raspberry Pi. Ground can be connected to
+// Connect the PWM and DIR pins to the mbed. The motor driver's fault signals
+// should go to the Raspberry Pi. Ground can be connected to
 // either.
 // 
+// Datasheet: https://www.pololu.com/product/755
+//
 // Sample usage:
-//   Motor motor(p21, p11, p12);
-//   motor.drive(0.5)  // Runs motor forward at 50% speed.
+//   Motor motor(p21, p11);
+//   motor.drive(0.5)   // Runs motor forward at 50% speed.
 //   motor.drive(-0.5)  // Runs motor backwards at 50% speed.
 class Motor {
   public:    
     // Initialize the motor.
     //
     // Args:
-    //   pin_enable: The motor driver's enable pin. In order to have PWM output,
-    //     the pin should be between 21 and 26
-    //   pin_pos: The motor driver's PWMP pin.
-    //   pin_neg: The motor driver's PWMN pin.
-    Motor(PinName pin_enable, PinName pin_pos, PinName pin_neg)
-        : enable(pin_enable), pos(pin_pos),  neg(pin_neg) {
-      enable = pos = neg = 0;  // Set all outputs to low.
-      enable.period_us(40);  // Set PWM output frequency to 25 kHz.
+    //   pin_pwm: The motor driver's PWM pin. In order to have PWM output,
+    //     the pin should be between 21 and 26.
+    //   pin_dir: The motor driver's DIR pin. HI is forward, LO is reverse.
+    Motor(PinName pin_pwm, PinName pin_dir) : pwm(pin_pwm), dir(pin_dir) {
+      pwm = dir = 0;  // Set all outputs to low.
+      pwm.period_us(40);  // Set PWM output frequency to 25 kHz.
     }
 
     // Drive the motor at the given speed.
     //
-    // This works by setting PWMP and PWMN to either high or low depending on
-    // the direction the motor turns, and outputting a PWM signal to the motor
-    // driver's enable pin.
-    //
     // Args:
     //   speed: A value between -1 and 1, representing the speed of the motor.
     void drive(float speed) {
-      pos = (speed < 0 ? 0 : 1);
-      neg = (speed < 0 ? 1 : 0);
-      enable = abs(speed);
+      dir = speed < 0 ? 0 : 1;
+      pwm = abs(speed);
     }
     
   private:
     // Variables:
-    //   enable: PwmOut for the motor driver's enable pin.
-    //   pos: DigitalOut for the motor driver's PWMP pin.
-    //   neg: DigitalOut for the motor driver's PWMN pin.
-    PwmOut enable;
-    DigitalOut pos, neg;
+    //   pwm: PwmOut for the motor driver's PWM pin.
+    //   dir: DigitalOut for the motor driver's direction pin.
+    PwmOut pwm;
+    DigitalOut dir;
 };
 
 
 int main() {
-  // The four motors are in an array. The raspberry pi expects this order, so
-  // do not change it without changing the code for the pi as well. Note that
-  // in the prototype robot, the polarity of the right wheel has been flipped.
-  Motor motors[4] = { Motor(p21, p11, p12),    // Left wheel
-                      Motor(p22, p13, p14),    // Right wheel
-                      Motor(p23, p27, p28),    // Left flipper
-                      Motor(p24, p29, p30) };  // Right flipper
+  // The four motors are in an array. The raspberry pi expects this order;
+  // do not change it without changing the code for the rpi as well.
+  Motor motors[4] = { Motor(p21, p11),    // Left wheels
+                      Motor(p22, p12),    // Right wheels
+                      Motor(p23, p13),    // Left flipper
+                      Motor(p24, p14) };  // Right flipper
 
   AnalogIn pots[2] = {p15,   // Left flipper position
                       p16};  // Right flipper position
@@ -98,6 +91,6 @@ int main() {
     motors[packet.b.motor_id].drive(sign * packet.b.speed / 31.0);
     
     // Send data to Rpi
-    rpi.printf("0x%04X 0x%04X", pots[0].read_u16(), pots[1].read_u16())
+    rpi.printf("0x%04X 0x%04X", pots[0].read_u16(), pots[1].read_u16());
   }
 }
