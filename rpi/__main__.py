ee16d72c4f0661a41d90713a3eb356230a07eb0a
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-02-25 02:12:09 +0900

    Add flippers and register mbed

--- rpi/__main__.py
+++ rpi/__main__.py
@@ -21,15 +21,20 @@ def main():
         client = Client(("10.249.255.151", 9999))
 
     logging.debug("Initializing motors")
-    left_motor = Motor("left_motor", 11, max_speed=0.6)
-    right_motor = Motor("right_motor", 38, max_speed=0.6)
+    left_motor = Motor("left_motor", 11, 12, max_speed=0.6)
+    right_motor = Motor("right_motor", 15, 16, max_speed=0.6)
+    left_flipper = Motor("left_flipper", 31, 32, max_speed=0.6)
+    right_flipper = Motor("right_flipper", 35, 36, max_speed=0.6)
 
     try:
         logging.debug("Connecting mbed")
         mbed_ser = serial.Serial("/dev/ttyACM0", 9600)
+        client.add_serial_device("mbed_flipper", mbed_ser)
         logging.debug("Registering motors to client")
         client.add_motor(left_motor, ser=mbed_ser)
         client.add_motor(right_motor, ser=mbed_ser)
+        client.add_motor(left_flipper, ser=mbed_ser)
+        client.add_motor(right_flipper, ser=mbed_ser)
     except serial.SerialException:
         logging.warning("The mbed is not connected!")
 
