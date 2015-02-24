a9dd4796c064fc2b0a45a8669c8b738b2c1118ef
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-02-24 22:58:50 +0900

    Update exceptions.py

--- common/exceptions.py
+++ common/exceptions.py
@@ -2,12 +2,10 @@
 # Released under the GNU General Public License, version 3
 class KaginawaException(Exception):
     """Root for all Kaginawa exceptions. Never raised."""
-    pass
 
 
 class CRCError(KaginawaException):
     """Raised when a cyclic redundancy check fails."""
-    pass
 
 class DriverError(KaginawaException):
     """Raised when a motor has no drivers enabled."""
