ffa35004f95325602d3d5623202c597c50b07148
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-03-19 19:57:19 +0900

    Update client.py

--- opstn/client.py
+++ opstn/client.py
@@ -1,1 +1,19 @@
+# (C) 2015 Kyoto University Mechatronics Laboratory
+# Released under the GNU General Public License, version 3
+"""
+UDP server for sensor data.
 
+"""
+import logging
+import pickle
+
+from common.networking import UDPClientBase
+
+class Client(UDPClientBase):
+    def run(self):
+        self.logger.info("Client started")
+        
+        self.send("\n", self.server_address)
+        while True:
+            result = self.receive(64)
+            self.logger.debug(str(pickle.loads(result)))
