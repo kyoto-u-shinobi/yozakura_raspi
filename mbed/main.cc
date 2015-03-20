69aebec72b890e0f6c3893f4f008f1a953192822
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-03-21 01:59:30 +0900

    Separate pin declarations.

--- mbed/main.cc
+++ mbed/main.cc
@@ -2,23 +2,11 @@
 // Released under the GNU General Public License, version 3
 #include "mbed.h"
 #include "motor.h"
+#include "pins.h"
 
 Serial rpi(USBTX, USBRX);  // USB port acts as a serial connection with rpi.
 
 int main() {
-    // The four motors are in an array. The raspberry pi expects this order;
-    // do not change it without changing the code for the rpi as well.
-    Motor motors[4] = { Motor(p21, p11),   // Left wheels
-                        Motor(p22, p12),   // Right wheels
-                        Motor(p23, p13),   // Left flipper
-                        Motor(p24, p14) }; // Right flipper
-    
-    // Set up ADC ports. If you add ADC devices, please add them in order
-    // from the lowest pin to the highest.
-    AnalogIn pots[6] = {p15, p16, p17, p18,  // Unused 
-                        p19,                 // Left flipper position
-                        p20};                // Right flipper position
-    
     int n_adc = 2; // Number of ADC Channels in use. Max is 6.
     uint16_t adc_results[n_adc];
     for (int i = 0; i < n_adc; i++) {
