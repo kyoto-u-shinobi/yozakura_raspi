4aedcee995f49fc6e5f9b0d8497e80355fbe9aec
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-03-28 16:10:12 +0900

    Bring back automatic I2C bus identification.

--- rpi/devices.py
+++ rpi/devices.py
@@ -9,6 +9,7 @@ each attached device.
 """
 from collections import OrderedDict
 import logging
+import re
 import subprocess
 
 from RPi import GPIO as gpio
@@ -20,7 +21,38 @@ from common.exceptions import BadArgError, I2CSlotBusyError, \
 from rpi.bitfields import CurrentConfiguration, CurrentAlerts
 
 
-i2c_bus = 1
+def get_i2c_bus():
+    """
+    Detect the revision number of a Raspberry Pi, useful for changing functionality
+    like default I2C bus based on revision. The revision list is available online.
+    [1]_
+    
+    Revision 1 pi uses I2C Bus 0, while revision 2 uses I2C Bus 1.
+    
+    This function is based on an Adafruit I2C implementation. [2]_
+    
+    References
+    ----------
+    .. [1] ELinux.org. "RPi Hardware History."
+           http://elinux.org/RPi_HardwareHistory#Board_Revision_History
+    .. [2] Adafruit Python GPIO, Adafruit, Github. "Platform.py"
+           https://github.com/adafruit/Adafruit_Python_GPIO/blob/master/Adafruit_GPIO/Platform.py#L53
+    
+    """
+    with open('/proc/cpuinfo', 'r') as infile:
+        for line in infile:
+            # Match a line of the form "Revision : 0002" while ignoring extra
+            # info in front of the revsion (like 1000 when the Pi was over-volted).
+            match = re.match('Revision\s+:\s+.*(\w{4})$', line, flags=re.IGNORECASE)
+            if match and match.group(1) in ['0000', '0002', '0003']:  # Revision 1
+                return 0
+            elif match:  # Revision 2
+                return 1
+        # Couldn't find the revision, throw an exception.
+        raise RuntimeError('Could not determine Raspberry Pi revision.')
+
+
+i2c_bus = get_i2c_bus()  # /dev/i2c-0 or /dev/i2c-1
 
 
 def get_used_i2c_slots(bus_number=i2c_bus):
