528ea8651f637f9f4d02620dfcaaefb5933119e5
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-04-22 22:29:54 +0900

    Identify mbeds properly

--- rpi/mbed.py
+++ rpi/mbed.py
@@ -6,6 +6,7 @@ Functions for use with the mbeds.
 """
 import logging
 import serial
+import time
 
 from common.exceptions import NoMbedError, UnknownMbedError,\
     YozakuraTimeoutError
@@ -14,35 +15,48 @@ from rpi.bitfields import MotorPacket
 
 
 def connect_to_mbeds():
+    mbed_arm = None
+    mbed_body = None
+    
     try:
         mbed = serial.Serial("/dev/ttyACM0", baudrate=38400)
     except serial.SerialException:
         raise NoMbedError
-
     try:
         reply = _identify_mbed(mbed)
     except YozakuraTimeoutError:
-        mbed_arm = mbed
-        arm_at_zero = True
+        raise UnknownMbedError
     else:
-        if reply == "body":
+        if reply == "arm":
+            mbed_arm = mbed
+        elif reply == "body":
             mbed_body = mbed
-            arm_at_zero = False
         else:
             raise UnknownMbedError
 
-    if arm_at_zero:
-        try:
-            mbed_body = serial.Serial("/dev/ttyACM1", baudarate=38400)
-        except serial.SerialException:
-            raise NoMbedError  # Body mbed not attached.
-    else:
-        try:
-            mbed_arm = serial.Serial("/dev/ttyACM1", baudarate=38400)
-        except serial.SerialException:
+    try:
+        mbed = serial.Serial("/dev/ttyACM1", baudarate=38400)
+    except serial.SerialException:
+        if mbed_body is not None:
             logging.warning("Arm mbed is not attached!")
-            mbed_arm = None
-
+        else:
+            raise NoMbedError  # Body mbed not attached.
+    try:
+        reply = _identify_mbed(mbed)
+    except YozakuraTimeoutError:
+        raise UnknownMbedError
+    else:
+        if reply == "arm":
+            if mbed_arm is not None:
+                raise UnknownMbedError("Multiple arm mbeds are attached")
+            mbed_arm = mbed
+        elif reply == "body":
+            if mbed_body is not None:
+                raise UnknownMbedError("Multiple body mbeds are attached")
+            mbed_body = mbed
+        else:
+            raise UnknownMbedError
+            
     return mbed_arm, mbed_body
 
 
@@ -55,4 +69,5 @@ def _identify_mbed(ser):
     id_request.speed = 0
 
     ser.write(bytes([id_request.as_byte]))
+    time.sleep(0.01);
     return ser.read(ser.inWaiting()).decode().split("\n")[-2]
