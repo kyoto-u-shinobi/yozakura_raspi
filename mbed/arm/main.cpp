4a456be4ca9a00fe3231c53236bae159e5723181
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-04-30 18:27:13 +0900

    Sensors only; no servos.

--- mbed/arm/main.cpp
+++ mbed/arm/main.cpp
@@ -1,179 +1,114 @@
-#include <vector>
+// (C) 2015 Kyoto University Mechatronics Laboratory
+// Released under the GNU General Public License, version 3
 #include "mbed.h"
-#include "Dynamixel.h"
-#include "MEMS.h"
 
-struct ArmPacketBits {
-                      unsigned int mode : 2;
-                      unsigned int linear : 2;
-                      unsigned int pitch : 2;
-                      unsigned int yaw : 2;
-                     };
 
-union ArmPacket {
-                 struct ArmPacketBits b;
-                 unsigned char as_byte;
-                };
+Serial rpi(USBTX, USBRX); // tx, rx
 
-Serial rpi(USBTX, USBRX);
 
-AnalogIn co2(p20);
+// Omron D6T-44L-06 (4x4) sensors. [1]_
+//
+// References:
+//   .. [1] Omron, D6T-44L / D6T-8L Application note.
+//          http://www.omron.com/ecb/products/sensor/special/mems/pdf/AN-D6T-01EN_r2.pdf
+I2C thermo_sensors[2] = { I2C(p9, p10),     // Left sensor
+                          I2C(p28, p27) };  // Right sensor
 
-DigitalOut dx_low(p16);
-DigitalOut dx_relay(p18);
+// Fis SB-AQ6B CO2 and Tobacco sensor. [2]_
+//
+// The sensor can read up to 3000 ppm.
+//
+// Notes:
+//   Concentration of carbon dioxide in outside air is typically around
+//   396 ppm, while exhaled air can go up to 13200 ppm. This sensor can only
+//   read up to 3000 ppm.
+// 
+// References:
+//   .. [2] Fis SB-AQ6B CO2 Monitoring Module Datasheet.
+//          http://www.fisinc.co.jp/common/pdf/A051020-AQ6.pdf
+AnalogIn co2(p17);
 
-std::vector<Dynamixel*> servos;
-AX12 *linear = new AX12(p13, p14, 0);
-MX28 *pitch = new MX28(p13, p14, 1);
-MX28 *yaw = new MX28(p13, p14, 2);
+double temperatures[2][16] = {0};       // 16 temperatures for each sensor.
+double past_temperatures[2][16] = {0};  // 16 temperatures for each sensor.
 
-MEMS thermo_sensors[] = { MEMS(p9, p10),
-                          MEMS(p28, p27) };
 
-int minima[] = {100, 172, 360};
-int maxima[] = {300, 334, 360};
-float speeds[] = {0.1, 0.2, 0.2};
-int inits[] = {maxima[0], maxima[1], 0};
-int goals[] = {maxima[0], maxima[1], 0};
-
-void DxGoHome() {
-  linear->SetGoal(inits[0]);
-  if (not linear->isMoving()) {
-    pitch->SetGoal(inits[1]);
-    yaw->SetGoal(inits[2]);
-  }
-}
-
-void DxInitialize() {
-  dx_low = 0;
-  dx_relay = 1;
-  
-  for (int i=0; i < 3; i++) {
-    servos[i]->SetCWLimit(minima[i]);
-    servos[i]->SetCCWLimit(maxima[i]);
-    servos[i]->SetCRSpeed(speeds[i]);
-  }
-}
-
-void DxReset() {
-  dx_relay = 0;
-  wait_ms(10);
-  DxInitialize();
-}
-
-void DxEnd() {
-  DxGoHome();
-  while (linear->isMoving()) {}
-  DxGoHome();
-  while (pitch->isMoving() or yaw->isMoving()) {}
-  dx_relay = 0;
-}
-
+// Return the measured CO2 value.
 float GetCO2() {
-  return co2.read() * 5000 + 400;  // ppm
+    return co2.read() * 5000 + 400;  // ppm
 }
+
+
+// Update the temperatures.
+//
+// The temperatures are stored in two global variables, `temperatures` and
+// `past_temperatures`. If the temperature returned by a thermo sensor is
+// outside the allowable range (defined as 5 to 50 degrees), reuse data
+// from the previous read.
+//
+// The two arrays are updated by reference.
+void UpdateTemperatures() {
+  uint8_t thermo_address = 0x14;
+  uint8_t thermo_start_read = 0x4c;
+  
+  char raw_data[35];
+  short raw_temps[16];
+  
+  int itemp;
+  
+  for (int sensor_number = 0; sensor_number < 2; sensor_number++) {
+    thermo_sensors[sensor_number].start();
+    thermo_sensors[sensor_number].write(thermo_address);
+    thermo_sensors[sensor_number].write(thermo_start_read);
+    thermo_sensors[sensor_number].read(thermo_address, raw_data, 35);
+  
+    for (int i = 0; i < 17; i++) {
+      itemp = (raw_data[2*i] & 0xff);
+      itemp += raw_data[2*i + 1] * 256;
+      if (i > 0) {
+        raw_temps[i-1] = itemp;
+      }
+    }
+  
+    for (int i = 0; i < 16; i++) {
+      if (raw_temps[i] > 50 && raw_temps[i] < 500) {  // 5 to 50 degrees
+        temperatures[sensor_number][i] = 0.1 * raw_temps[i];
+        past_temperatures[sensor_number][i] = temperatures[sensor_number][i];
+      } else {
+        temperatures[sensor_number][i] = past_temperatures[sensor_number][i];
+      }
+    }
+  }
+}
+
 
 int main() {
-  servos.push_back(linear);
-  servos.push_back(pitch);
-  servos.push_back(yaw);
-  
-  float positions[3];
-  float values[3];
-  float thermo_data[2][16];
   float co2_data;
-  int commands[3];
-
-  union ArmPacket packet;
+  char packet;
 
   rpi.baud(38400);  // Match this in the RPi settings.
 
-  DxInitialize();  // Comment this out when testing without the arm.
-  DxGoHome();
-  
   while (1) {
-    packet.as_byte = rpi.getc();
+    if (rpi.readable()) {
+      packet = rpi.getc();
 
-    for (int i = 0; i < 3; i++) {
-      positions[i] = -1;
-      values[i] = -1;
-    }
-
-    commands[0] = packet.b.linear;
-    commands[1] = packet.b.pitch;
-    commands[2] = packet.b.yaw;
-    
-    switch (packet.b.mode) {
-      case 0: {
-        if (not dx_relay) {
-          break;
-        }
-        
-        linear->SetTorqueLimit(1);  // Reset torque limit
-        for (int i=0; i < 3; i++) {
-          positions[i] = servos[i]->GetPosition();
-          if (i == 0) {
-            values[i] = servos[i]->GetVolts();
-          } else {
-            values[i] = servos[i]->GetCurrent();
-          }
-          
-          goals[i] = positions[i];
-          if (commands[i] == 1) {
-            goals[i]++;
-          } else if (commands[i] == 2) {
-            goals[i]--;
-          }
-          servos[i]->SetGoal(goals[i]);
-        }
-        break;
-      } case 1: {
-        if (dx_relay) {
-          DxGoHome();
-        }
-        break;
-      } case 2: {
-        DxReset();
-        break;
-      } case 3: {
-        if (packet.b.linear) {
-          rpi.printf("arm\n");
-        } else {
-          if (dx_relay) {
-            DxEnd();
-          }
-        }
-        break;
+      if (packet == 7) {      // 7 is a request for identification.
+	rpi.printf("arm\n");  // mbed name.
+	continue;
       }
     }
-    
+
+    // Update data values.
+    UpdateTemperatures();
+    co2_data=GetCO2();
+
+    // Send thermo data.
     for (int i=0; i < 2; i++) {
-      thermo_sensors[i].temp(thermo_data[i]);
-    }
-    
-    co2_data = GetCO2();
-    
-    // Send Dynamixel position
-    for (int i=0; i < 3; i++) {
-      rpi.printf("%4.1f ", positions[i]);
-    }
-    
-    // Send Dynamixel values
-    for (int i=0; i < 3; i++) {
-      rpi.printf("%4.1f ", values[i]);
-    }
-    
-    // Send thermo data
-    for (int i=0; i < 2; i++) {
-      for (int j=0; i < 16; i++) {
-        rpi.printf("%4.1f ", thermo_data[i][j]);
+      for (int j=0; j < 16; j++) {
+	rpi.printf("%4.1f ", temperatures[i][j]);
       }
     }
-    
-    // Send CO2 data
-    rpi.printf("%4.1f", co2_data);
-    
-    // End transmission
-    rpi.printf("\n");
+                
+    // Send CO2 data and end line.
+    rpi.printf("%4.1f\n", co2_data);
   }
 }
