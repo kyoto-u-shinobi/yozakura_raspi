88622e2af84601f42edfdbbcd976b6ed0e944282
masasin <mshtsy+github@gmail.com>
2015-05-12 14:19:34 +0900

    Clean up root directory.
    Signed-off-by: masasin <mshtsy+github@gmail.com>

--- ping2ttyACM1.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-s = serial.Serial("/dev/ttyACM1", 38400)
-s.write(bytes([7]))
-try:
-    print(s.read(s.inWaiting()).decode())
-finally:
-    s.close()
--- ping_mbed.py
+++ ping_mbed.py
@@ -1,58 +1,27 @@
+# (C) 2015  Kyoto University Mechatronics Laboratory
+# Released under the GNU General Public License, version 3
+"""
+A script to check for the identities and locations of any connected mbeds.
+
+This script usually needs to be run with superuser privileges.
+
+"""
 import serial
 import time
 
-print("Connecting to /dev/ttyACM0...", end="\r")
-try:
-    s = serial.Serial("/dev/ttyACM0", 38400)
-    s.write(bytes([7]))
-    time.sleep(1)
-    print("The mbed at /dev/ttyACM0 says: " + s.read(s.inWaiting()).decode())
-except serial.SerialException:
-    print("No mbed connected to /dev/ttyACM0")
-finally:
+for i in range(4):
+    device = "/dev/ttyACM{n}".format(n=i)
+    print("Connecting to {dev}...".format(dev=device), end="\r")
     try:
-        s.close()
-    except NameError:
-        pass
-
-print("Connecting to /dev/ttyACM1...", end="\r")
-try:
-    s = serial.Serial("/dev/ttyACM1", 38400)
-    s.write(bytes([7]))
-    time.sleep(1)
-    print("The mbed at /dev/ttyACM1 says: " + s.read(s.inWaiting()).decode())
-except serial.SerialException:
-    print("No mbed connected to /dev/ttyACM1")
-finally:
-    try:
-        s.close()
-    except NameError:
-        pass
-
-print("Connecting to /dev/ttyACM2...", end="\r")
-try:
-    s = serial.Serial("/dev/ttyACM2", 38400)
-    s.write(bytes([7]))
-    time.sleep(1)
-    print("The mbed at /dev/ttyACM2 says: " + s.read(s.inWaiting()).decode())
-except serial.SerialException:
-    print("No mbed connected to /dev/ttyACM2")
-finally:
-    try:
-        s.close()
-    except NameError:
-        pass
-
-print("Connecting to /dev/ttyACM3...", end="\r")
-try:
-    s = serial.Serial("/dev/ttyACM3", 38400)
-    s.write(bytes([7]))
-    time.sleep(1)
-    print("The mbed at /dev/ttyACM3 says: " + s.read(s.inWaiting()).decode())
-except serial.SerialException:
-    print("No mbed connected to /dev/ttyACM3")
-finally:
-    try:
-        s.close()
-    except NameError:
-        pass
+        s = serial.Serial(device, 38400)
+        s.write(bytes([7]))  # mbed ID request
+        time.sleep(1)
+        reply = s.read(s.inWaiting()).decode()
+        print("The mbed at {dev} says: {reply}".format(dev=device, reply=reply))
+    except serial.SerialException:
+        print("No mbed connected to {dev}".format(dev=device))
+    finally:
+        try:
+            s.close()
+        except NameError:
+            pass
